e7b8acef798e2cb7b5b74a3ddb9dc19122124e58
Serkan <k.serkanertas@gmail.com>
2021-02-14 17:04:46 +0100

    added more options to nav

--- navbar.js
+++ navbar.js
@@ -1,11 +1,23 @@
 const root = document.querySelector('html');
 const burger = document.querySelector('#burger');
+const more_options = document.querySelector('#more_options');
+const more_options_menu = document.querySelector('#more_options_menu');
 const menu = document.querySelector('#menu');
 
 burger.addEventListener('click', () => {
   if (menu.classList.contains('hidden')) {
     menu.classList.remove('hidden');
+    more_options_menu.classList.remove('hidden');
   } else {
     menu.classList.add('hidden');
+    more_options_menu.classList.add('hidden');
+  }
+})
+
+more_options.addEventListener('click', () => {
+  if (more_options_menu.classList.contains('hidden')) {
+    more_options_menu.classList.remove('hidden');
+  } else {
+    more_options_menu.classList.add('hidden');
   }
 })
